3b3d0521a1beb0380eb2e5631fa0fbbe87a9a294
konouluc01 <traefifi@gmail.com>
2026-07-18 22:06:45 +0000

    doc api en ligne 3

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
-require('ts-node/register');
-const { createNestApp } = require('../src/main');
+const { createNestApp } = require('../dist/src/main');
 
 let cachedApp = null;
 
 module.exports = async (req, res) => {
-  if (!cachedApp) {
-    cachedApp = await createNestApp();
+  try {
+    if (!cachedApp) {
+      cachedApp = await createNestApp();
+    }
+    cachedApp.getHttpAdapter().getInstance()(req, res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error', message: error.message });
   }
-  cachedApp.getHttpAdapter().getInstance()(req, res);
 };
